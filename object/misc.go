d0a92d259c72a55720264a78c0246d1cf2625f6c
Marcin <marcin.konow@lczyk.xyz>
2025-05-31 17:45:47 +0100

    more minor

--- object/misc.go
+++ object/misc.go
@@ -2,9 +2,26 @@ package object
 
 import (
 	"github.com/MarcinKonowalczyk/goruby/object/call"
+	"github.com/MarcinKonowalczyk/goruby/object/env"
 	"github.com/MarcinKonowalczyk/goruby/object/ruby"
 	"github.com/MarcinKonowalczyk/goruby/trace"
 )
+
+var (
+	CLASSES = env.NewEnvironment[ruby.Object]()
+	// symbol to which all function definitions are attached to
+	FUNCS_STORE = ruby.NewExtendedObject(FUNCS)
+)
+
+// NewMainEnvironment returns a new Environment populated with all Ruby classes
+// and the Kernel functions
+func NewMainEnvironment() env.Environment[ruby.Object] {
+	env := CLASSES.Clone()
+	env.Set("bottom", BOTTOM)
+	env.Set("funcs", FUNCS_STORE)
+	env.SetGlobal("$stdin", IoClass)
+	return env
+}
 
 func WithArity(arity int, fn ruby.Method) ruby.Method {
 	return ruby.NewMethod(
